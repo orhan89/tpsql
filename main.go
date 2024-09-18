f3bb9ecab25f27ebf1ee13e013a3ab11456e062b
Ricky Hariady <ricky@sense-os.nl>
2024-09-18 16:07:34 +0700

    try running psql

--- main.go
+++ main.go
@@ -1,9 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"os"
+	"os/exec"
 )
 
 func main() {
-	fmt.Println("Hello flake")
+	log.Print("Searching for psql binary")
+	_, err := exec.LookPath("psql")
+	if err != nil {
+		log.Fatal("psql binary is not available in PATH")
+	}
+
+	cmd := exec.Command("psql", "postgres")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	err = cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Print("Exiting")
 }
