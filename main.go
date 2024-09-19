a158c9869f9f3ca6099ef6209331242c06c6713a
Ricky Hariady <ricky@sense-os.nl>
2024-09-19 15:26:59 +0700

    get server host and port from psql argument

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net"
 	"os"
 	"os/exec"
+	"slices"
 	"strconv"
 	"syscall"
 	"time"
@@ -17,14 +18,35 @@ func main() {
 	localPort := 5432
 	tunnelHost := flag.String("sshHost", "127.0.0.1", "(ssh) tunnel host")
 	tunnelUser := flag.String("sshUser", "root", "(ssh) tunnel user")
-	serverHost := flag.String("host", "127.0.0.1", "postgresql server host")
-	serverPort := flag.Int("port", 5432, "postgresql server port")
-	serverUser := flag.String("username", "postgres", "postgresql server user")
-	serverDatabase := flag.String("dbname", "postgres", "postgresql database name")
+	serverHost := "127.0.0.1"
+	serverPort := 5432
 
 	flag.Parse()
+	psqlArgs := flag.Args()
 
-	portForwardingAddress := fmt.Sprintf("%s:%d:%s:%d", localHost, localPort, *serverHost, *serverPort)
+	if i := slices.Index(psqlArgs, "--host"); i != -1 {
+		serverHost = psqlArgs[i+1]
+		log.Printf("Server Host: %s", serverHost)
+
+		psqlArgs = slices.Delete(psqlArgs, i, i+2)
+	}
+
+	psqlArgs = slices.Concat([]string{"--host", localHost}, psqlArgs)
+
+	if i := slices.Index(psqlArgs, "--port"); i != -1 {
+		serverPort, err := strconv.Atoi(psqlArgs[i+1])
+		if err != nil {
+			log.Print("Failed in parsing server port")
+			return
+		}
+		log.Printf("Server Port: %d", serverPort)
+
+		psqlArgs = slices.Delete(psqlArgs, i, i+2)
+	}
+
+	psqlArgs = slices.Concat([]string{"--port", strconv.Itoa(localPort)}, psqlArgs)
+
+	portForwardingAddress := fmt.Sprintf("%s:%d:%s:%d", localHost, localPort, serverHost, serverPort)
 	tunnelAddress := fmt.Sprintf("%s@%s", *tunnelUser, *tunnelHost)
 	log.Print(portForwardingAddress)
 	log.Print(tunnelAddress)
@@ -78,7 +100,8 @@ func main() {
 	}
 
 	log.Print("Start Psql")
-	cmdPsql := exec.Command("psql", "-h", localHost, "-p", strconv.Itoa(localPort), "-U", *serverUser, *serverDatabase)
+	log.Printf("Arguments for psql command: %v", psqlArgs)
+	cmdPsql := exec.Command("psql", psqlArgs...)
 	cmdPsql.Stdin = os.Stdin
 	cmdPsql.Stdout = os.Stdout
 	cmdPsql.Stderr = os.Stderr
