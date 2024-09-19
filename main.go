f849c60b1105e223a3409e380c905185782972c2
Ricky Hariady <ricky@sense-os.nl>
2024-09-19 14:50:41 +0700

    run psql

--- main.go
+++ main.go
@@ -7,8 +7,12 @@ import (
 )
 
 func main() {
-	log.Print("Searching for psql binary")
-	_, err := exec.LookPath("psql")
+	serverHost := flag.String("host", "127.0.0.1", "postgresql server host")
+	serverPort := flag.Int("port", 5432, "postgresql server port")
+	serverUser := flag.String("username", "postgres", "postgresql server user")
+	serverDatabase := flag.String("dbname", "postgres", "postgresql database name")
+
+	flag.Parse()
 	if err != nil {
 		log.Fatal("psql binary is not available in PATH")
 	}
@@ -22,5 +26,25 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	log.Print("Searching for psql binary")
+	_, err = exec.LookPath("psql")
+	if err != nil {
+		log.Print("psql binary is not available in PATH")
+		return
+	}
+
+	log.Print("Start Psql")
+	cmdPsql := exec.Command("psql", "-h", serverHost, "-p", strconv.Itoa(serverPort), "-U", *serverUser, *serverDatabase)
+	cmdPsql.Stdin = os.Stdin
+	cmdPsql.Stdout = os.Stdout
+	cmdPsql.Stderr = os.Stderr
+
+	err = cmdPsql.Run()
+	if err != nil {
+		log.Print(err)
+		return
+	}
+
 	log.Print("Exiting")
 }
