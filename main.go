855283b2695f903b4200f828b3d328fa156d5023
Ricky Hariady <ricky@sense-os.nl>
2024-09-20 14:11:32 +0700

    refactor tunnel management

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"log"
@@ -13,41 +14,21 @@ import (
 	"time"
 )
 
-func main() {
-	localHost := "127.0.0.1"
-	localPort := 5432
-	tunnelHost := flag.String("sshHost", "127.0.0.1", "(ssh) tunnel host")
-	tunnelUser := flag.String("sshUser", "root", "(ssh) tunnel user")
-	serverHost := "127.0.0.1"
-	serverPort := 5432
+type SSHTunnel struct {
+	localHost string
+	localPort int
+	remoteHost string
+	remotePort int
+	remoteUser string
+	postgresHost string
+	postgresPort int
 
-	flag.Parse()
-	psqlArgs := flag.Args()
+	cmd *exec.Cmd
+}
 
-	if i := slices.Index(psqlArgs, "--host"); i != -1 {
-		serverHost = psqlArgs[i+1]
-		log.Printf("Server Host: %s", serverHost)
-
-		psqlArgs = slices.Delete(psqlArgs, i, i+2)
-	}
-
-	psqlArgs = slices.Concat([]string{"--host", localHost}, psqlArgs)
-
-	if i := slices.Index(psqlArgs, "--port"); i != -1 {
-		serverPort, err := strconv.Atoi(psqlArgs[i+1])
-		if err != nil {
-			log.Print("Failed in parsing server port")
-			return
-		}
-		log.Printf("Server Port: %d", serverPort)
-
-		psqlArgs = slices.Delete(psqlArgs, i, i+2)
-	}
-
-	psqlArgs = slices.Concat([]string{"--port", strconv.Itoa(localPort)}, psqlArgs)
-
-	portForwardingAddress := fmt.Sprintf("%s:%d:%s:%d", localHost, localPort, serverHost, serverPort)
-	tunnelAddress := fmt.Sprintf("%s@%s", *tunnelUser, *tunnelHost)
+func (s *SSHTunnel) Connect() error {
+	portForwardingAddress := fmt.Sprintf("%s:%d:%s:%d", s.localHost, s.localPort, s.postgresHost, s.postgresPort)
+	tunnelAddress := fmt.Sprintf("%s@%s", s.remoteUser, s.remoteHost)
 	log.Print(portForwardingAddress)
 	log.Print(tunnelAddress)
 
@@ -55,26 +36,24 @@ func main() {
 	_, err := exec.LookPath("ssh")
 	if err != nil {
 		log.Print("ssh binary is not available in PATH")
-		return
+		return err
 	}
 
-	cmd := exec.Command("ssh", "-N", "-L", portForwardingAddress, tunnelAddress)
-	cmd.Stdin = os.Stdin
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
+	s.cmd = exec.Command("ssh", "-N", "-L", portForwardingAddress, tunnelAddress)
+	s.cmd.Stdin = os.Stdin
+	s.cmd.Stdout = os.Stdout
+	s.cmd.Stderr = os.Stderr
 
 	log.Print("Opening tunnel")
-	err = cmd.Start()
+	err = s.cmd.Start()
 	if err != nil {
 		log.Print(err)
-		return
+		return err
 	}
-
-	defer cmd.Process.Signal(syscall.SIGTERM)
 
 	log.Print("Waiting until tunnel is open")
 
-	address := net.JoinHostPort(localHost, strconv.Itoa(localPort))
+	address := net.JoinHostPort(s.localHost, strconv.Itoa(s.localPort))
 
 	connected := false
 	for i := 0; i < 10; i++ {
@@ -89,9 +68,68 @@ func main() {
 
 	if connected == false {
 		log.Print("Tunnel connection timeout. Exiting.")
-		return
+		s.Close()
+		return errors.New("Timeout")
 	}
 
+	return nil
+}
+
+func (s *SSHTunnel) Close() error {
+	s.cmd.Process.Signal(syscall.SIGTERM)
+	return nil
+}
+
+func main() {
+	localHost := "127.0.0.1"
+	localPort := 5432
+	// tunnelType := flag.String("tunnelType", "ssh", "the type of the tunnel (default=ssh)")
+	tunnelHost := flag.String("sshHost", "127.0.0.1", "(ssh) tunnel host")
+	tunnelUser := flag.String("sshUser", "root", "(ssh) tunnel user")
+	postgresHost := "127.0.0.1"
+	postgresPort := 5432
+
+	flag.Parse()
+	psqlArgs := flag.Args()
+
+	if i := slices.Index(psqlArgs, "--host"); i != -1 {
+		postgresHost = psqlArgs[i+1]
+		log.Printf("Server Host: %s", postgresHost)
+
+		psqlArgs = slices.Delete(psqlArgs, i, i+2)
+	}
+
+	psqlArgs = slices.Concat([]string{"--host", localHost}, psqlArgs)
+
+	if i := slices.Index(psqlArgs, "--port"); i != -1 {
+		postgresPort, err := strconv.Atoi(psqlArgs[i+1])
+		if err != nil {
+			log.Print("Failed in parsing server port")
+			return
+		}
+		log.Printf("Server Port: %d", postgresPort)
+
+		psqlArgs = slices.Delete(psqlArgs, i, i+2)
+	}
+
+	psqlArgs = slices.Concat([]string{"--port", strconv.Itoa(localPort)}, psqlArgs)
+
+	tunnel := &SSHTunnel{
+		localHost: localHost,
+		localPort: localPort,
+		remoteHost: *tunnelHost,
+		remoteUser: *tunnelUser,
+		postgresHost: postgresHost,
+		postgresPort: postgresPort,
+	}
+
+	err := tunnel.Connect()
+	if err != nil {
+		panic("error connecting to tunnel")
+	}
+	
+	defer tunnel.Close()
+	
 	log.Print("Searching for psql binary")
 	_, err = exec.LookPath("psql")
 	if err != nil {
